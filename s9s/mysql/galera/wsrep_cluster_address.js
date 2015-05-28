27910a2ab2a73cb38cb72751ba44237f9558ce0e
Johan Andersson <johan@severalnines.com>
2015-05-28 11:45:48 +0000

    improved advisor

--- s9s/mysql/galera/wsrep_cluster_address.js
+++ s9s/mysql/galera/wsrep_cluster_address.js
@@ -27,32 +27,59 @@ function main()
         var config      = host.config();
         var variable = config.variable("wsrep_cluster_address");
         var value = variable[0]["value"];
+        var origValue  = value;
+        origValue.replace('"',"");
         value.replace("'","");
         value.replace("gcomm://","");
         var address = value.split(",");
         var found=true;
+        var missingHosts = "";
         for (k = 0; k < hosts.size(); k++)
         {
             if (!address.contains(hosts[k].hostName()))
             {
                 found=false;
-                advice.setSeverity(1);
-                advice.setJustification("wsrep_cluster_address=" + value + 
-                                        " does not contain " + 
-                                        hosts[k].hostName());
-                advice.setAdvice("Add " + hosts[k].hostName() + 
-                                 
-                                 " to wsrep_cluster_address.");
+                advice.setSeverity(Warning);
+                if(k < hosts.size()-1)  
+                    missingHosts = missingHosts + hosts[k].hostName() + " ";
+                else
+                    missingHosts = missingHosts + hosts[k].hostName();
             }
         }
         if (found)
         {
-            advice.setSeverity(0);
-            advice.setJustification("wsrep_cluster_address=" + value + 
+            advice.setSeverity(Ok);
+            advice.setJustification("wsrep_cluster_address=" + origValue + 
                                     " contains all galera nodes.");
             advice.setAdvice("No action needed.");
+        }
+        else
+        {
+            missingHosts.replace(" ", ",");
+            var justification="";
+            var msg = "";
+            if(origValue == "gcomm://")
+            {
+                advice.setSeverity(Critical);
+                msg = "Set wsrep_cluster_adress=gcomm://" + missingHosts;
+                justification = "wsrep_cluster_address=gcomm:// ."
+                    " This can lead to disasters and data loss.";
+            }
+            else
+            {
+                msg = "Add " + missingHosts + 
+                      " to wsrep_cluster_address." + 
+                      " Set wsrep_cluster_address=" + 
+                    origValue + "," +  missingHosts;
+                justification = "wsrep_cluster_address=" + origValue + 
+                    " does not contain " + missingHosts;
+            }
+            print(host.hostName() + ": " + msg);
+            advice.setJustification(justification);
+            advice.setAdvice(msg);   
         }
         advisorMap[idx]= advice;
     }
     return advisorMap;
 }
+
