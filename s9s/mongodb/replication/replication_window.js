1b7a479673cdc354f8351826381f77b69e215fdd
Art <art@dbart.net>
2016-10-10 16:02:36 +0200

    Fixed the two MongoDB scripts: one was broken after the RunMongoQuery got updated and the other ran ineffeciently

--- s9s/mongodb/replication/replication_window.js
+++ s9s/mongodb/replication/replication_window.js
@@ -1,4 +1,3 @@
-
 #include "common/helpers.js"
 #include "cmon/io.h"
 #include "cmon/alarms.h"
@@ -28,52 +27,57 @@ function main(hostAndPort) {
     var replwindow = 0;
     for (i = 0; i < hosts.size(); i++)
     {
-        // Find the master and execute the queries there
+        // Find the primary and execute the queries there
         host = hosts[i];
         res = host.executeMongoQuery("admin", "{isMaster: 1}");
         if (res["result"]["ismaster"] == true) {
             master_host = host;
             optime_master = 0;
             optime_nodes = [];
-            res = host.executeMongoQuery("local", '{aggregate: "oplog.rs", pipeline: [{$group: {"_id": null, min: {$min: "$ts"}, max: {$max: "$ts"}}}]}');
-            max = res["result"]["result"][0]["max"]["$timestamp"]["t"];
-            min = res["result"]["result"][0]["min"]["$timestamp"]["t"];
+
+            // Fetch the first and last record from the Oplog and take its timestamp
+            res = host.executeMongoQuery("local", '{find: "oplog.rs", sort: { $natural: 1}, limit: 1}');
+            min = res["result"]["cursor"]["firstBatch"][0]["ts"]["$timestamp"]["t"];
+            res = host.executeMongoQuery("local", '{find: "oplog.rs", sort: { $natural: -1}, limit: 1}');
+            max = res["result"]["cursor"]["firstBatch"][0]["ts"]["$timestamp"]["t"];
             replwindow = max - min;
-            
+
             res = host.executeMongoQuery("admin", "{ replSetGetStatus: 1 }");
-            // Fetch the optime per host
+            // Fetch the optime and uptime per host
             for(o = 0; o < res["result"]["members"].size(); o++)
             {
                 node_status = res["result"]["members"][o];
-                // Keep reference to the master host
-                if (node_status["name"] == master_host)
-                {
-                    optime_master = o;
-                }
                 optime_nodes[o] = {};
                 optime_nodes[o]["name"] = node_status["name"];
+                optime_nodes[o]["uptime"] = node_status["uptime"];
                 optime_nodes[o]["optime"] = node_status["optime"]["ts"]["$timestamp"]["t"];
                 // replication window of each node is last committed transaction minus the first item in the oplog of the master
                 optime_nodes[o]["replwindow"] = node_status["optime"]["ts"]["$timestamp"]["t"]-min;
-                
-                    
             }
-            msg = ADVICE_OK;
+        }
+    }
+
+    for (i = 0; i < optime_nodes.size(); i++)
+    {
+        msg = ADVICE_OK;
+        host = optime_nodes[i]["name"];
+        if(optime_nodes[i]["uptime"] < WARNING_REPL_WINDOW)
+        {
+            msg = "Host " + optime_nodes[i]["name"] + " only has an uptime of " + optime_nodes[i]["uptime"] + " seconds. Too early to determine the replication window.";
+            advice.setSeverity(Ok);
+        }
+        else {
             // Check if any of the hosts is within the oplog window
-            for(o = 0; o < optime_nodes.size(); o++)
+            if(optime_nodes[i]["replwindow"] < WARNING_REPL_WINDOW)
             {
-                if(optime_nodes[optime_master]["replwindow"] < WARNING_REPL_WINDOW)
-                {
-                    advice.setSeverity(Warning);
-                    msg = ADVICE_WARNING + "Host " + optime_nodes[o]["name"] + " has a replication window of " + optime_nodes[optime_master]["replwindow"] + " seconds.";
-                    advice.setJustification(JUSTIFICATION_WARNING);
-                }
-            }
-            
-            if (advice.severity() <= 0) {
+                advice.setSeverity(Warning);
+                msg = ADVICE_WARNING + "Host " + optime_nodes[i]["name"] + " has a replication window of " + optime_nodes[optime_master]["replwindow"] + " seconds.";
+                advice.setJustification(JUSTIFICATION_WARNING);
+            } else {
                 advice.setSeverity(Ok);
             }
         }
+
         advice.setHost(host);
         advice.setTitle(TITLE);
         advice.setAdvice(msg);
@@ -81,4 +85,3 @@ function main(hostAndPort) {
     }
     return advisorMap;
 }
-
